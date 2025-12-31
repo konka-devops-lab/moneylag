415910694630cfd2c453c33f5f880ddf4dc09a73
siva <sivaram0434@gmail.com>
2025-12-31 13:17:35 +0530

    update the usernames and passwords

--- mysql/schema.sql
+++ mysql/schema.sql
@@ -1,5 +1,5 @@
-CREATE DATABASE IF NOT EXISTS crud_app;
-USE crud_app;
+CREATE DATABASE IF NOT EXISTS moneylag_app;
+USE moneylag_app;
 
 CREATE TABLE entries (
   id BIGINT AUTO_INCREMENT PRIMARY KEY,
@@ -8,6 +8,6 @@ CREATE TABLE entries (
   date DATE NOT NULL  
 );
 
-CREATE USER IF NOT EXISTS 'crud'@'%' IDENTIFIED BY 'CrudApp@1';
-GRANT ALL ON crud_app.* TO 'crud'@'%';
+CREATE USER IF NOT EXISTS 'moneylag'@'%' IDENTIFIED BY 'moneylagApp@1';
+GRANT ALL ON moneylag_app.* TO 'moneylag'@'%';
 FLUSH PRIVILEGES;
